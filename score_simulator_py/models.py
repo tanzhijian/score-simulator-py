00ac2a9a85a12e6cd8b3b185c7786a4c8bf7c7c6
tanzhijian <i@tanzhijian.org>
2023-12-10 20:53:58 +0800

    refactor types

--- score_simulator_py/models.py
+++ score_simulator_py/models.py
@@ -1,29 +1,3 @@
-from typing import TypedDict
-
-
-class _Competition(TypedDict):
-    name: str
-    logo: str
-
-
-class _Team(TypedDict):
-    name: str
-    logo: str
-    shots: int
-    xg: float
-    score: int | None
-    played: int
-
-
-class Match(TypedDict):
-    name: str
-    utc_time: str
-    finished: bool
-    competition: _Competition
-    home: _Team
-    away: _Team
-
-
 class GameState:
     def __init__(self) -> None:
         self._reset()
